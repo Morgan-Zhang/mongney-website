cb045526579fcdcf78d8f8796da4921aba8ce5e0
morgan <zhangteng_yx@126.com>
2022-02-25 21:58:43 +0800

    deploy

---
+++ precache-manifest.a7150665b47db04412ac46f56644517f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ee53b27ff675b1ce6a",
+    "url": "/mongney-website/css/app.ea2617ed.css"
+  },
+  {
+    "revision": "f6953b439d5739c3eaa2d51ab11cc01a",
+    "url": "/mongney-website/index.html"
+  },
+  {
+    "revision": "49ee53b27ff675b1ce6a",
+    "url": "/mongney-website/js/app.3655a2a8.js"
+  },
+  {
+    "revision": "e0c97a8eb5c95061dc7c",
+    "url": "/mongney-website/js/chunk-vendors.9dabad09.js"
+  },
+  {
+    "revision": "fe000ebaafafedf6d7c0a1de95510103",
+    "url": "/mongney-website/manifest.json"
+  },
+  {
+    "revision": "eea4aa2a5de96e50ed8c917668dd0373",
+    "url": "/mongney-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mongney-website/robots.txt"
+  }
+]);
